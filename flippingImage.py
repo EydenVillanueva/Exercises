01284307e87be267b2f315eb038a2b52f2ca94f8
eyden <eydenv85@gmail.com>
2019-08-20 10:56:07 -0500

    flippingImage

---
+++ flippingImage.py
@@ -0,0 +1,22 @@
+def flippingImage(matrix):
+    if not matrix:
+        return matrix
+
+    for i, row in enumerate(matrix):
+        row.reverse()
+        for j in range(len(row)):
+            if row[j] == 1:
+                row[j] = 0
+            else:
+                row[j] = 1
+        matrix[i] = row
+    
+    return matrix
+
+
+if __name__ == "__main__":
+    print(flippingImage([
+        [1,1,0],
+        [1,1,0],
+        [1,1,0],
+    ]))
